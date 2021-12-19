f62289c4239eba37c7c4b3b5c2f8bdaea7754eb5
kid7258 <kid7258@naver.com>
2021-12-19 17:08:18 +0900

    준영속 상태 - clear 동작 확인

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,7 +18,9 @@ public class JpaMain {
             Member member = em.find(Member.class, 150L);
             member.setName("AAAAA");
 
-            em.detach(member);
+            em.clear();
+
+            Member member2 = em.find(Member.class, 150L);
 
             System.out.println("=========");
             tx.commit();
